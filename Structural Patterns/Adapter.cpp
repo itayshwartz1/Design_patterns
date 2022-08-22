db141f5f10aecbc1977330333f4fb80a237048e7
Itay Shwartz <71848366+itayshwartz1@users.noreply.github.com>
2022-08-22 15:22:14 +0300

    Update Adapter.cpp

--- Structural Patterns/Adapter.cpp
+++ Structural Patterns/Adapter.cpp
@@ -1,13 +1,18 @@
+// We use this pattern when we need to adapt between one object to another. 
+// what we need to do is to create class "Adapter" thet *contain* the "Adaptee" and *inherit* "Task". 
+// If we make it inherit from Adaptee then for each combination of them we will have to open a new class
+// In this the adapter is some kind of sorting, and we want to do some specific task with the sorting. 
+
 #include <iostream>
-using namespace std;
 #include "algorithm"
 #include <vector>
 
+using namespace std;
 
 class Adaptee{
 public:
     std::vector<int> vector;
-    Adaptee(std::vector<int> vector){
+    Adaptee(std::vector<int> &vector){
         this->vector = vector;
     }
     Adaptee(){}
@@ -50,12 +55,11 @@ public:
     }
 };
 
-//we use example that the adapter is some kind of sorting, and we want to do some specific task on the sorting.
-//int main() {
-//    Adaptee sorter = Adaptee({9,6,2,5,7,0,1,3,4,8});
-//    Adapter adapter(sorter);
-//    adapter.doSortAndRequest();
-//    adapter.print();
-//    return 0;
-//}
+int main() {
+    Adaptee sorter = Adaptee({9,6,2,5,7,0,1,3,4,8});
+    Adapter adapter(sorter);
+    adapter.doSortAndRequest();
+    adapter.print();
+    return 0;
+}
 
