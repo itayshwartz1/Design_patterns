3dee8889155f1bd2e469165a049a80730ab19b11
Itay Shwartz <71848366+itayshwartz1@users.noreply.github.com>
2022-08-22 20:07:11 +0300

    Update Interpreter.cpp

--- Behavioral Patterns/Interpreter.cpp
+++ Behavioral Patterns/Interpreter.cpp
@@ -1,17 +1,19 @@
+// Interpreter design pattern is one of the behavioral design pattern. Interpreter pattern is used to defines a
+// grammatical representation for a language and provides an interpreter to deal with this grammar.
+
 #include <iostream>
 #include "list"
 
-
 class TreeNode{
 public:
-    virtual int interprete() = 0;
+    virtual int interpret() = 0;
 };
 
 class Leaf : public TreeNode{
 public:
     int num;
     Leaf(int num) : num(num){}
-    int interprete() override{
+    int interpret() override{
         return num;
     }
 };
@@ -26,41 +28,39 @@ public:
 class AddNode : public BinaryNode {
 public:
     AddNode(TreeNode *left, TreeNode *right) : BinaryNode(left, right) {}
-    int interprete() override{
-        return left->interprete() + right->interprete();
+    int interpret() override{
+        return left->interpret() + right->interpret();
     }
 };
 
 class SubNode : public BinaryNode {
 public:
     SubNode(TreeNode *left, TreeNode *right) : BinaryNode(left, right) {}
-    int interprete() override{
-        return left->interprete() - right->interprete();
+    int interpret() override{
+        return left->interpret() - right->interpret();
     }
 };
 
 class MultNode : public BinaryNode {
 public:
     MultNode(TreeNode *left, TreeNode *right) : BinaryNode(left, right) {}
-    int interprete() override{
-        return left->interprete() * right->interprete();
+    int interpret() override{
+        return left->interpret() * right->interpret();
     }
 };
 
 class DivNode : public BinaryNode {
 public:
     DivNode(TreeNode *left, TreeNode *right) : BinaryNode(left, right) {}
-    int interprete() override{
-        return left->interprete() / right->interprete();
+    int interpret() override{
+        return left->interpret() / right->interpret();
     }
 };
 
 int main(){
+    // (1+7) * (4/(3-1)) = 16
     TreeNode * treeNode = new MultNode(new AddNode(new Leaf(1), new Leaf(7)),
                                        new DivNode(new Leaf(4), new SubNode(new Leaf(3), new Leaf(1))));
-    std::cout << treeNode->interprete();
+    std::cout << treeNode->interpret() << std::endl;
     return 0;
 }
-
-//(1+7) * (4/(3-1)) = 16
-
