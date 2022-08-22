35f90953dad2f58137bc9649684709f8be7bcab5
Itay Shwartz <71848366+itayshwartz1@users.noreply.github.com>
2022-08-22 19:19:50 +0300

    Update Singleton.cpp

--- Creational Patterns/Singleton.cpp
+++ Creational Patterns/Singleton.cpp
@@ -1,19 +1,26 @@
+//Singleton is a creational design pattern that lets you ensure that a class has only one instance,
+// while providing a global access point to this instance.
+
 #include <iostream>
 using namespace std;
 
 class Instance {
     friend class Singleton;
     Instance(){
-        cout << "From Builder";
+        cout << "From Builder" <<endl;
     }
 };
 
+// static make the Instance create one time and don't die.
 class Singleton{
     static Instance* instance;
 public:
     static Instance* getInstance(){
         if (!instance)
             instance = new Instance();
+        else{
+            cout << "Do you come here a lot? It seems that the Instance has already been initialized" <<endl;
+        }
         return instance;
     }
 };
@@ -22,6 +29,8 @@ Instance* Singleton::instance = nullptr;
 
 
 int main() {
-    Instance* instance = Singleton::getInstance();
-    instance = Singleton::getInstance();
+    Instance* instance1 = Singleton::getInstance();
+    Instance* instance2 = Singleton::getInstance();
+    Instance* instance3 = Singleton::getInstance();
+    Instance* instance4 = Singleton::getInstance();
 }
