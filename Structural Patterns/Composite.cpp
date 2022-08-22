7221e47a94dbd58decdfb47444cdda183aeaf9ca
Itay Shwartz <71848366+itayshwartz1@users.noreply.github.com>
2022-08-22 17:49:33 +0300

    Update Composite.cpp

--- Structural Patterns/Composite.cpp
+++ Structural Patterns/Composite.cpp
@@ -1,3 +1,6 @@
+// This pattern help us to maintain a hierarchical structure. Each component can contain Components or a Leafs. Leaf not contain anyone.
+// The Component and the Leaf both inheritor from Component.
+
 #include "vector"
 #include "string"
 #include <list>
@@ -12,39 +15,41 @@ public:
         this->name = name;
     }
 
-    void add(Component *component){
+    void add(Component* component){
         list.push_back(component);
     }
 
-    void remove(Component *component){
+    void remove(Component* &component){
         list.remove(component);
     }
 
-    virtual void getChild(){}
+    virtual void getChild() const = 0;
 
-    virtual void operation(){}
+    virtual void operation() const= 0;
 
 };
 
 class Composite : public Component{
 public:
     Composite(string name): Component(name){}
-    virtual void getChild(){
-        cout << name;
+    virtual void getChild() const{
+        cout << name << endl;
         for(auto &c: list){
             c->getChild();
         }
     }
-    virtual void operation() override{}
+    virtual void operation() const override {}
 };
 
 class Leaf : public Component{
 public:
     Leaf(string name): Component(name){}
-    virtual void getChild(){
-        cout << name;
+    virtual void getChild() const{
+        cout << name << endl;
     }
-    virtual void operation() override{}
+    virtual void operation() const override {
+        //do something
+    }
 };
 
 int main(){
@@ -52,6 +57,7 @@ int main(){
     Composite c1("a");
     c1.add(new Composite("b"));
     c1.add(new Leaf("c"));
+    c1.getChild();
 
     return 0;
 }
